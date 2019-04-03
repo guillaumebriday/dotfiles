331a1693ce2c0e179432c0f100ace0befa0ec464
Guillaume Briday <guillaumebriday@gmail.com>
2019-04-03 22:17:13 +0200

    Updating vscode extensions

--- vscode/vscode.sh
+++ vscode/vscode.sh
@@ -4,7 +4,6 @@
 find ~/dotfiles/vscode -name '*.json' | xargs -I file ln -fs file ~/Library/Application\ Support/Code/User/
 
 # Packages
-code --install-extension aki77.rails-partial
 code --install-extension alefragnani.project-manager
 code --install-extension atipugin.rails-partial
 code --install-extension bmewburn.vscode-intelephense-client
@@ -13,6 +12,7 @@ code --install-extension BriteSnow.vscode-toggle-quotes
 code --install-extension bung87.rails
 code --install-extension bung87.vscode-gemfile
 code --install-extension bungcip.better-toml
+code --install-extension christian-kohler.npm-intellisense
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension CraigMaslowski.erb
 code --install-extension cssho.vscode-svgviewer
@@ -20,18 +20,17 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension formulahendry.auto-close-tag
 code --install-extension formulahendry.auto-rename-tag
-code --install-extension ginfuru.ginfuru-vscode-jekyll-syntax
-code --install-extension ginfuru.vscode-jekyll-snippets
 code --install-extension haaaad.ansible
+code --install-extension hollowtree.vue-snippets
 code --install-extension HookyQR.beautify
 code --install-extension Hridoy.rails-snippets
 code --install-extension karunamurti.haml
 code --install-extension marcostazi.VS-code-vagrantfile
+code --install-extension mechatroner.rainbow-csv
 code --install-extension miguel-savignano.ruby-symbols
 code --install-extension mikestead.dotenv
 code --install-extension mrmlnc.vscode-apache
 code --install-extension neilbrayfield.php-docblocker
-code --install-extension neilding.language-liquid
 code --install-extension octref.vetur
 code --install-extension onecentlin.laravel-blade
 code --install-extension PeterJausovec.vscode-docker
@@ -39,6 +38,8 @@ code --install-extension PKief.material-icon-theme
 code --install-extension pnp.polacode
 code --install-extension possan.nbsp-vscode
 code --install-extension rebornix.ruby
+code --install-extension redhat.vscode-yaml
+code --install-extension ritwickdey.LiveServer
 code --install-extension robinbentley.sass-indented
 code --install-extension shanehofstetter.rails-i18n
 code --install-extension shanoor.vscode-nginx
@@ -46,12 +47,14 @@ code --install-extension sianglim.slim
 code --install-extension sporto.rails-go-to-spec
 code --install-extension steoates.autoimport
 code --install-extension sysoev.language-stylus
+code --install-extension TomasHubelbauer.vscode-markdown-table-format
+code --install-extension tomoki1207.pdf
 code --install-extension Tyriar.sort-lines
 code --install-extension usernamehw.indent-one-space
-code --install-extension viatsko.theme-base16-ocean-dark
 code --install-extension vscoss.vscode-ansible
 code --install-extension wayou.vscode-todo-highlight
 code --install-extension whatwedo.twig
 code --install-extension wmaurer.change-case
 code --install-extension yzhang.markdown-all-in-one
+code --install-extension zhuangtongfa.Material-theme
 code --install-extension Zignd.html-css-class-completion
