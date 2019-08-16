6cb4bf48ef8c2db778cf565d911fea6a9931acb4
Guillaume Briday <guillaumebriday@gmail.com>
2019-08-16 17:19:26 +0200

    Updating vscode snippets config

--- vscode/vscode.sh
+++ vscode/vscode.sh
@@ -2,7 +2,7 @@
 
 # Configuration files
 find ~/dotfiles/vscode -name '*.json' | xargs -I file ln -fs file ~/Library/Application\ Support/VSCodium/User/
-find ~/dotfiles/vscode/snippets -type f | xargs -I file ln -fs file ~/Library/Application\ Support/VSCodium/User/snippets
+ln -fs ~/dotfiles/vscode/snippets/ ~/Library/Application\ Support/VSCodium/User
 
 # Packages
 code --install-extension alefragnani.project-manager
