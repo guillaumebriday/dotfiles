b895f3c88991a5721feec1791dc50670b569edf4
Guillaume Briday <guillaumebriday@gmail.com>
2020-05-09 14:34:57 +0200

    Updating vscode extensions

--- vscode/vscode.sh
+++ vscode/vscode.sh
@@ -10,7 +10,6 @@ packages=(
   atipugin.rails-partial
   bmewburn.vscode-intelephense-client
   bradlc.vscode-tailwindcss
-  BriteSnow.vscode-toggle-quotes
   bung87.rails
   bung87.vscode-gemfile
   bungcip.better-toml
@@ -20,6 +19,7 @@ packages=(
   CraigMaslowski.erb
   cssho.vscode-svgviewer
   dbaeumer.vscode-eslint
+  DotJoshJohnson.xml
   eamodio.gitlens
   formulahendry.auto-close-tag
   formulahendry.auto-rename-tag
@@ -34,9 +34,12 @@ packages=(
   mikestead.dotenv
   mrmlnc.vscode-apache
   ms-azuretools.vscode-docker
+  ms-vsliveshare.vsliveshare
   neilbrayfield.php-docblocker
+  noku.rails-run-spec-vscode
   octref.vetur
   onecentlin.laravel-blade
+  Orta.vscode-jest
   PKief.material-icon-theme
   pnp.polacode
   possan.nbsp-vscode
@@ -44,7 +47,6 @@ packages=(
   redhat.vscode-yaml
   ritwickdey.LiveServer
   robinbentley.sass-indented
-  sdras.night-owl
   sdras.vue-vscode-snippets
   shanehofstetter.rails-i18n
   shanoor.vscode-nginx
@@ -52,6 +54,7 @@ packages=(
   sporto.rails-go-to-spec
   steoates.autoimport
   sysoev.language-stylus
+  timonwong.shellcheck
   TomasHubelbauer.vscode-markdown-table-format
   tomoki1207.pdf
   Tyriar.sort-lines
@@ -59,10 +62,10 @@ packages=(
   vscoss.vscode-ansible
   wayou.vscode-todo-highlight
   whatwedo.twig
-  wix.vscode-import-cost
+  wingrunr21.vscode-ruby
   wmaurer.change-case
   yzhang.markdown-all-in-one
-  zhuangtongfa.Material-theme
+  zhuangtongfa.material-theme
   Zignd.html-css-class-completion
 )
 
