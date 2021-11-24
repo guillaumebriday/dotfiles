7cbe40d2aeb1cd48ac13d9be39af6c15a8510f6d
Guillaume Briday <guillaumebriday@gmail.com>
2021-11-25 00:14:57 +0100

    Use Ruby 2.7.5

--- ruby/rbenv.sh
+++ ruby/rbenv.sh
@@ -5,8 +5,8 @@ ln -fs ~/dotfiles/ruby/.gemrc ~/
 ln -fs ~/dotfiles/ruby/.irbrc ~/
 
 # Installing and setting latest version of Ruby
-rbenv install 2.7.4
-rbenv global 2.7.4
+rbenv install 2.7.5
+rbenv global 2.7.5
 
 # Installing default gems
 gem install bundler rails awesome_print
