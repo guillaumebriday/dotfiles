4177ac56ca5935c1d025859e36971eaf5b0f721d
Guillaume Briday <8252238+guillaumebriday@users.noreply.github.com>
2021-12-09 10:17:37 +0100

    Fixing oh my zsh url

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Configuration files
 ln -fs ~/dotfiles/zsh/.zshrc ~/
